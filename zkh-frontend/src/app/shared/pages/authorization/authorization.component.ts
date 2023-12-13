5aa69ba1d407ae1f7e2729aa5b284f1850951c07
Alex <alex_morozov2603@mail.ru>
2023-12-13 22:46:00 +0300

    ups

--- zkh-frontend/src/app/shared/pages/authorization/authorization.component.ts
+++ zkh-frontend/src/app/shared/pages/authorization/authorization.component.ts
@@ -28,52 +28,52 @@ export class AuthorizationComponent {
 
 
   login() {
-    // this.requestService.login(this.authForm.value.username, this.authForm.value.password)
-    // .subscribe(
-    //   response => {
-    //     this.authService.logedIn = true
-    //     this.authService.id = response.id
-    //     this.authService.name = response.name
-    //     this.authService.phone = response.phoneNumber
-    //     this.authService.address = response.address
-    //     this.authService.role = response.role
-    //     this.zone.run(() => { 
-    //       switch (this.authService.role) {
-    //         case Role.WORKER:
-    //           this.router.navigate(["/worker-tasks"])
-    //           break;
-    //         case Role.TENANT:
-    //           this.router.navigate(["/tenant-appeal-list"])
-    //           break;
-    //         case Role.MANAGER:
-    //           this.router.navigate(["/manager-all-appeals"])
-    //           break;
-    //       }
-    //     })
-    //   },
-    //   err => {
-    //     this.authForm.reset()
-    //   }
-    // )
-    this.authService.logedIn = true
-    this.authService.id = 1
-    this.authService.name = "Иванов Иван Иванович"
-    this.authService.phone = "8-962-169-35-12"
-    this.authService.address = undefined
-    this.authService.role = Role.MANAGER
-    this.zone.run(() => { 
-      switch (this.authService.role) {
-        case Role.WORKER:
-          this.router.navigate(["/worker-tasks"])
-          break;
-        case Role.TENANT:
-          this.router.navigate(["/tenant-appeal-list"])
-          break;
-        case Role.MANAGER:
-          this.router.navigate(["/manager-all-appeals"])
-          break;
+    this.requestService.login(this.authForm.value.username, this.authForm.value.password)
+    .subscribe(
+      response => {
+        this.authService.logedIn = true
+        this.authService.id = response.id
+        this.authService.name = response.name
+        this.authService.phone = response.phoneNumber
+        this.authService.address = response.address
+        this.authService.role = response.role
+        this.zone.run(() => { 
+          switch (this.authService.role) {
+            case Role.WORKER:
+              this.router.navigate(["/worker-tasks"])
+              break;
+            case Role.TENANT:
+              this.router.navigate(["/tenant-appeal-list"])
+              break;
+            case Role.MANAGER:
+              this.router.navigate(["/manager-all-appeals"])
+              break;
+          }
+        })
+      },
+      err => {
+        this.authForm.reset()
       }
-    })
+    )
+    // this.authService.logedIn = true
+    // this.authService.id = 1
+    // this.authService.name = "Иванов Иван Иванович"
+    // this.authService.phone = "8-962-169-35-12"
+    // this.authService.address = undefined
+    // this.authService.role = Role.MANAGER
+    // this.zone.run(() => { 
+    //   switch (this.authService.role) {
+    //     case Role.WORKER:
+    //       this.router.navigate(["/worker-tasks"])
+    //       break;
+    //     case Role.TENANT:
+    //       this.router.navigate(["/tenant-appeal-list"])
+    //       break;
+    //     case Role.MANAGER:
+    //       this.router.navigate(["/manager-all-appeals"])
+    //       break;
+    //   }
+    // })
   }
 
   submitOnEnter(event: KeyboardEvent) {
